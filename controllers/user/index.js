b8b32b97d1112ed803fe2c9594367e96db82d0f3
lylelljr <lylelljr@gmail.com>
2019-08-11 07:40:17 -0700

    Remove information from error message.

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -11,7 +11,7 @@ router.get('/:id', checkAuthorization, async (req, res, next) => {
 
     const user = await userService.getByID(id);
     if (!user) {
-      return res.status(404).json({ message: `User ${id} not found` });
+      return res.status(404).json({ message: `User was not found` });
     }
 
     return res.status(200).json(user);
